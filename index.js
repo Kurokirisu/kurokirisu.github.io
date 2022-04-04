cff3843f589fee0ae3e66878e9500a0404e02c40
juliemachiels <j.machiels@innervate.nl>
2022-04-04 16:04:58 +0200

    Added Node, started refactor

---
+++ index.js
@@ -0,0 +1,7 @@
+const database = require("./assets/character_database.json");
+
+const Web = require( "webwebweb" );
+// Web.APIs[ "/" ] = ( qs, body, opts ) => {
+//     return { "database": database };
+//   };
+Web.Run( 8080 );
